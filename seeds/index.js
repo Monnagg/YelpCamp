5e588675333f2a8b307c49d91eb3f40167353b30
Monnagg <Jiangmenghua1992@gmail.com>
2023-01-14 18:50:24 -0500

    step5. insert 50 campground documents

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,11 @@
-//1.	连接mongodb，引入Campground
+//1.连接并测试数据库
 const mongoose  = require("mongoose");
+//2.引入Campground Model
 const Campground = require('../models/campground');
+//引入cities
+const cities = require("./cities");
+//引入places and descriptor
+const {places, descriptors} = require('./seedHelper');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser:true,
@@ -12,13 +17,25 @@ db.once('open',()=>{
     console.log('Dabase connected');
 });
 
-//2、测试是否连接数据库
+const title= array=> array[Math.floor(Math.random()*array.length)];
+
+//3.向数据库插入50个campground的信息
 const seedDB = async() =>{
-    //delete all documents in Campground model（collection）
-    //Modele.deleteMany({})方法
     await Campground.deleteMany({});
-    const c = new Campground({title:'purple field'});
-    await c.save();
+    for(let i=0; i<50;i++){
+        //array里有1000个city，从中随机抽取
+        const rand = Math.floor(Math.random()*1000);
+        
+        const camp = new Campground({
+            location:`${cities[rand].city}, ${cities[rand].state}`,
+            //descriptors和places是两个数组
+            title:`${title(descriptors)} ${title(places)}`
+        })
+        await camp.save();
+    }
+    
 }
 
-seedDB();
+seedDB().then(()=>{
+    mongoose.connection.close();
+});
